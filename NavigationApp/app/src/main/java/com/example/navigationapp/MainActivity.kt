dd632ddfa11dd318784d994af9b4e014ab4614b1
Yash raj <124022190+Yaahrit@users.noreply.github.com>
2024-03-23 19:51:17 +0530

    simple
    created a navigation app

---
+++ NavigationApp/app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.navigationapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationapp.ui.theme.NavigationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstScreen") {
+        // Define your navigation destinations here
+        composable("firstscreen") {
+            FirstScreen { name ->
+                navController.navigate("secondscreen \n $name")
+            }
+        }
+        composable("secondscreen/{name}") {
+            val name = it.arguments?.getString("name") ?: "No Name found"
+            SecondScreen(name) {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
---
+++ NavigationApp/app/src/main/java/com/example/navigationapp/SecondScreen.kt
@@ -0,0 +1,34 @@
+package com.example.navigationapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SecondScreen(name: String, navigationToFirstScreen: (String) -> Unit) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(text = "This is the Second Screen", fontSize = 24.sp)
+        Text(text = "Welcome $name", fontSize = 24.sp)
+        Button(onClick = {
+            navigationToFirstScreen(name)
+        }) {
+            Text(text = "Go to First screen")
+        }
+    }
+}
