be40bab312594a7f41119be643c36587007af65c
Yash raj <124022190+Yaahrit@users.noreply.github.com>
2024-04-07 19:51:07 +0530

    by java
    created a AlaramClock

---
+++ Alarmclock2/app/src/main/java/com/nisha/alarmclock/AlarmReceiver.java
@@ -0,0 +1,35 @@
+package com.nisha.alarmclock;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pd = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "androidknowledge")
+                .setSmallIcon(R.drawable.baseline_notifications_24)
+                .setContentTitle("Reminder")
+                .setContentText("It's time to wake up")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pd);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        notificationManagerCompat.notify(123, builder.build());
+    }
+}
