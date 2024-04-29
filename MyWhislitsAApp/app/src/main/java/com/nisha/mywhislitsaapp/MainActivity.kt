a9a1ec1904935f1e5adbce35bbcee8418d7410fc
Yaahrit <124022190+Yaahrit@users.noreply.github.com>
2024-04-29 20:04:01 +0530

    Jetpack compose project
    Database project of jetpack compose
    with Room Database

---
+++ MyWhislitsAApp/app/src/main/java/com/nisha/mywhislitsaapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nisha.mywhislitsaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.nisha.mywhislitsaapp.ui.theme.MyWhislitsAAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyWhislitsAAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation()
+                }
+            }
+        }
+    }
+}
+
